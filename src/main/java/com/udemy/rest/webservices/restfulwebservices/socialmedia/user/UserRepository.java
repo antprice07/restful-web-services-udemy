323bcec96ce8e866f541c0092f603baa384c4bf7
antprice07 <antprice07@gmail.com>
2023-02-01 14:56:30 -0500

    limits-service init commit

--- src/main/java/com/udemy/rest/webservices/restfulwebservices/socialmedia/user/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.udemy.rest.webservices.restfulwebservices.socialmedia.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer>
-{
-}
